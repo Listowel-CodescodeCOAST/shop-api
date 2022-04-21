0c18da611e7d6a50211cfa7883d639a1e9f1a192
Listowel-CodescodeCOAST <adusarkodielistowel@yahoo.com>
2022-04-21 14:01:07 +0000

    auth

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const { dbConnect } = require("./config/dbConnect");
 const productRouter = require("./routes/products.router");
+const authRouter = require("./routes/auth.router")
 
 const app = express();
 app.use(express.json());
 
+app.use("/auth", authRouter)
 app.use("/products", productRouter)
 
-const start = () => {
-     dbConnect();
+const start = async () => {
+    await dbConnect();
 
 app.listen(4000, () => {
     console.log("Server up and running");
